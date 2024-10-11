8a91ea13fda5bf78ad4522df8fef4f093c1e3d52
Shibo Lyu <github@of.sb>
2024-10-12 00:05:06 +0800

    refactor: remove sqlite & drizzle
    We will just use idb and localstorage to keep web client simple.

--- src/lib/db/common.ts
+++
@@ -1,5 +0,0 @@
-import { SQLocalDrizzle } from 'sqlocal/drizzle';
-import { drizzle } from 'drizzle-orm/sqlite-proxy';
-
-const { driver, batchDriver } = new SQLocalDrizzle('database.sqlite3');
-export const db = drizzle(driver, batchDriver);
