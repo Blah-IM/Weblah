0314dda37708a589baeb7e6712fcfe0df287a0bb
Shibo Lyu <hi@lao.sb>
2025-04-14 03:41:56 +0800

    fix: Add GroupedListContent component to exports

--- src/lib/components/GroupedList.ts
+++ src/lib/components/GroupedList.ts
@@ -1,6 +1,13 @@
 import GroupedListItem from './GroupedList/GroupedListItem.svelte';
+import GroupedListContent from './GroupedList/GroupedListContent.svelte';
 import GroupedListInputItem from './GroupedList/GroupedListInputItem.svelte';
 import GroupedListSection from './GroupedList/GroupedListSection.svelte';
 import GroupedListContainer from './GroupedList/GroupedListContainer.svelte';
 
-export { GroupedListItem, GroupedListInputItem, GroupedListSection, GroupedListContainer };
+export {
+	GroupedListItem,
+	GroupedListContent,
+	GroupedListInputItem,
+	GroupedListSection,
+	GroupedListContainer
+};
